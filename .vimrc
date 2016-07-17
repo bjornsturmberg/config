ca0824cf2b70aebc728915438d8cc63440f05075
Bjorn Sturmberg <bjorn.sturmberg@mq.edu.au>
2016-07-17 14:45:55 +1000

    Configuration files for setting up ubuntu, just the way I like.

---
+++ .vimrc
@@ -0,0 +1,33 @@
+set nu
+imap uu <esc>
+nmap n j
+nmap e k
+nmap i l
+nmap s i
+"imap <C-S-v> <esc>pi
+"imap <C-s> <esc>ZZi
+nmap <C-i> i_<esc>r
+"nmap <C-a> a_<esc>r
+imap aa <esc>A
+imap ii <esc>I
+nmap K i<esc>
+nmap o :
+nmap oO :!
+nmap oo :w
+nmap OO :!make
+nmap Onn :n
+nmap Obb :N
+"nmap <C-a-d> :wq
+nmap <C-d> :q
+nmap Y yt$
+map <C-J> <C-W>j<C-W>_
+map <C-K> <C-W>k<C-W>_
+
+let mapleader=','
+map <leader>ew :e <C-R>=expand("%:p:h") . "/" <CR>
+map <leader>es :sp <C-R>=expand("%:p:h") . "/" <CR>
+map <leader>ev :vsp <C-R>=expand("%:p:h") . "/" <CR>
+map <leader>et :tabe <C-R>=expand("%:p:h") . "/" <CR>
+
+"set spell spelllang=en_au
+:highlight Comment ctermfg=green
---
+++ setup_ubuntu.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+
+## Check network manager
+#sudo nm-connection
+
+## Update repositories
+#sudo apt-get update
+
+## Upgrade Ubuntu version
+#sudo apt-get dist-upgrade
+
+# Basics
+sudo apt-get -y install vim screen rsync ssh git # git-flow
+
+# Media
+sudo apt-get -y install vlc inkscape nautilus-image-converter hugin xournal pdftk blender impressive
+sudo apt-get -y install flac lame alien enscript gimp gimp-data gimp-plugin-registry gimp-data-extras
+
+# codecs
+sudo apt-get -y install ubuntu-restricted-extras libavcodec-extra libde265
+
+# Utilities
+sudo apt-get -y install dconf-editor default-jdk dconf-tools gnome-disk-utility gnome-tweak-tool unity-tweak-tool
+sudo apt-get -y install rar unrar htop ppa-purge samba gparted openssh-server p7zip-full
+
+# Latex
+sudo apt-get -y install  texlive-full latexmk texlive-bibtex-extra
+
+# Numerics
+sudo apt-get -y install make gfortran libatlas-dev libsuitesparse-dev libstdc++5 gmsh cmake
+
+# Google drive integration in nautilus
+sudo apt-get -y install gnome-control-center gnome-online-accounts owncloud-client
+
+# python
+sudo apt-get -y install python-pip python-numpy python-scipy python-matplotlib
+sudo apt-get -y install ipython python-sympy python-nose python-sphinx
+
+## Install python packages
+sudo pip install restview joblib
+
+# Sublime Text 3
+sudo add-apt-repository ppa:webupd8team/sublime-text-3 -y
+sudo add-apt-repository ppa:webupd8team/y-ppa-manager -y
+# # Git-core
+# sudo add-apt-repository ppa:git-core/ppa -y
+# Chrome
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb https://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+sudo apt-get update
+sudo apt-get install sublime-text-installer google-chrome-stable
+
+# # Personal rc files
+# wget .bashrc
+# wget .vimrc .vimrc_qwerty
+# wget .screenrc
+# wget bjorn_style.mplstyle
+# wget sublime macros
+
+# # Insert ssh keys
+# echo "/usr/bin/keychain --quiet $HOME/.ssh/id_rsa" >> .bashrc
+# echo "source $HOME/.keychain/$HOSTNAME-shk" >> .bashrc
+
+# # Skype
+# sudo add-apt-repository "deb http://archive.canonical.com/ $(lsb_release -sc) partner"
+# sudo dpkg --add-architecture i386
+# sudo apt-get update
+# sudo apt-get install skype
+
+# master-pdf-editor go to http://code-industry.net/free-pdf-editor.php
